b03056c8ca6dc189a069abd06e44ce83121c4732
oskarziembrowicz <rakson2002@gmail.com>
2023-12-05 11:05:33 +0100

    Adding corss-validation

--- lb7/cross-validation-breast-cancer.py
+++ lb7/cross-validation-breast-cancer.py
@@ -24,12 +24,12 @@ x_test = scaler.transform(x_test)
 
 def create_model():
     model = tf.keras.models.Sequential([
-        # tf.keras.layers.Flatten(input_shape=(28, 28)),
-        # tf.keras.layers.Dense(128, activation='relu'),
-        # tf.keras.layers.Dropout(0.2),
-        # tf.keras.layers.Dense(10, activation='softmax')
-        tf.keras.layers.Dense(16, activation='relu', input_shape=(x_train.shape[1],)),
-        tf.keras.layers.Dense(1, activation='softmax')
+        tf.keras.layers.Flatten(input_shape=(32, 7)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation='softmax')
+        # tf.keras.layers.Dense(16, activation='relu', input_shape=(x_train.shape[1],)),
+        # tf.keras.layers.Dense(1, activation='softmax')
     ])
 
     model.compile(
@@ -38,6 +38,22 @@ def create_model():
         metrics=['accuracy']
     )
     return model
+
+num_folds = 5
+kfold = KFold(n_splits=num_folds, shuffle=True)
+
+fold_no = 1
+for train, test in kfold.split(x_train, y_train):
+    model = create_model()
+    print(f'Training for fold {fold_no} ...')
+
+    model.fit(x_train[train], y_train[train], epochs=5)
+
+    scores = model.evaluate(x_train[test], y_train[test], verbose=0)
+    print(f'Score for fold {fold_no}: '
+          f'{model.metrics_names[0]} of {scores[0]}; '
+          f'{model.metrics_names[1]} of {scores[1]*100}%')
+    fold_no += 1
 
 # model1 = create_model()
 # model1.fit(x_train, y_train, epochs=5)
